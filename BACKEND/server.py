15e140fe0f5b48a4e6ec90064d60d2b3acd51ef5
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2024-11-25 20:52:20 -0500

    Update server.py

--- BACKEND/server.py
+++ BACKEND/server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, send_file, request, redirect
 from os import path
 
 import _mailman
-from BACKEND import _seismic
 
 type_map = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7}
 
